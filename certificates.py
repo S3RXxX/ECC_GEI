7fec8dc87488f8e0e47e22a8dd63ac17f6873ace
Sergi Guimerà Roig <sergiguimeraroig@gmail.com>
2024-12-20 17:40:55 +0100

    Reordered

--- certificates.py
+++
@@ -1,9 +0,0 @@
-
-if __name__=="__main__":
-    pass
-
-    """
-    C: Count nº revoked certificates:
-        -cmd: openssl crl -in .\GEANTOVRSACA4.crl -text -noout | grep 'Serial Number:' | wc -l
-        -Result: 22307
-    """
