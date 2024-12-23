d9370d419ec5ff031e9a613e62865f48b797af7c
Sergi Guimerà Roig <sergiguimeraroig@gmail.com>
2024-12-23 11:55:13 +0100

    Firma válida

--- ECC.py
+++ ECC.py
@@ -2,31 +2,9 @@
 import os
 import sympy as sp
 from ecpy.curves import Point, Curve
-from ecpy.keys import ECPublicKey, ECPrivateKey
+from ecpy.keys import ECPublicKey
 from ecpy.ecdsa import ECDSA
 import hashlib
-from pyasn1.codec.der.decoder import decode
-from pyasn1.type.univ import Sequence
-
-
-def read_ASN(hex_string):
-    # Convert the hex string to bytes
-    encoded_bytes = bytes.fromhex(hex_string)
-    
-    # Decode the ASN.1 data
-    decoded_data, _ = decode(encoded_bytes, asn1Spec=Sequence())
-    
-    # Process the decoded data as needed
-    return int(decoded_data[0]), int(decoded_data[1])
-
-
-def convert_signature_to_bytes(f1, f2):
-    """
-    Converts integers f1 and f2 to ECPy-compatible signature format (tuple of bytes).
-    """
-    byte_length = (f1.bit_length() + 7) // 8  # Minimum number of bytes to represent f1
-    # byte_length = 35
-    return f1.to_bytes(byte_length, byteorder='big') + f2.to_bytes(byte_length, byteorder='big')
 
 
 def read_data(file_path='', m_path=""):
@@ -39,17 +17,19 @@ def read_data(file_path='', m_path=""):
         curve_name = file.readline().strip().split(" ")[1]
         
         # Q = (Qx, Qy)
-        # Q = file.readline().split(" ")[1:3]
-        # Q = (int(Q[0], 16), int(Q[1], 16))
         Q = file.readline().strip().split(" ")[1]
         if Q[0:2] == '04':
             mid = 8*8+2
             Qx = int(Q[2:mid],16)
             Qy = int(Q[mid:],16)
+        else:
+            raise AssertionError("Q no empieza por 04")
         Q = (Qx, Qy)
+        
 
-    #     # f (f1, f2)
+        # firma (f1, f2)
         f = file.readline().strip().split(" ")[1]
+        f = bytes.fromhex(f)
         
         
     sha256_hash = hashlib.sha256()
@@ -84,23 +64,17 @@ def point_order(point, curve):
             return n
 
 # Generar una clave privada y calcular la pública
-def generate_keypair(curve,Q, d):
+def generate_pubkey(Q):
     public_key = ECPublicKey(Q)
-    private_key = ECPrivateKey(d, curve)
-    return private_key, public_key
+    return public_key
 
 
 # Verificar una firma ECDSA
 def verify_ecdsa_signature(public_key, message, signature):
     ecdsa = ECDSA()
-    # hashed_message = int.from_bytes(sha256(message).digest(), byteorder='big')
     return ecdsa.verify(message, signature, public_key)
 
 def calcula_preambulo():
-    # preambulo = bytes([0x20] * 64)    
-    # s = "TLS 1.3, server CertificateVerify".encode("ascii")
-    # preambulo += s
-    # preambulo += bytes([0x00])
     preambulo = "20"*64
     s = "TLS 1.3, server CertificateVerify"
     for c in s:
@@ -109,40 +83,33 @@ def calcula_preambulo():
     return preambulo
 
 
-
 if __name__=="__main__":
     """
     TODO:
         - Preguntes: ordre del punt si ordre curva primer (q) n = 1, q?
         -
     """
-    # Inicializa la curva elíptica
+    # Leemos los datos sacados manualmente de Wireshark
     curve_name, (Qx, Qy), f, m = read_data(file_path="./DATA.txt", m_path="./m.bin")
-    f = int(f, 16)
-    byte_length = (f.bit_length() + 7) // 8
-    f = f.to_bytes(byte_length)
     
-    # print(type(m), m)
-    # print(type(f), len(f),f)
-    # f = read_ASN(f)
-    # f = convert_signature_to_bytes(f1, f2)
+    # calculamos el preámbulo del mensaje firmado 
     preambulo = calcula_preambulo()
-    # print(preambulo)
-    # sha256(preambulo+m)
+
+    # hacemos el hash externo
     sha256_hash = hashlib.sha256()
     message_bytes = bytes.fromhex(preambulo+m)
     sha256_hash.update(message_bytes)
     message = sha256_hash.hexdigest()
-    # print("message", message)
-    message = bytes.fromhex(message)
-    # print(type(f), len(f))
-    # print(f)
 
+    message = bytes.fromhex(message)
+
+    # Inicializamos la curva
     curve = Curve.get_curve(curve_name)
     G = curve.generator
+    # Calculamos el orden de la curva
     generator_order = curve_order(curve=curve)
 
-
+    # Respuestas a las preguntas:
     print()
     print("Apartado a:")
     print(f"Orden del generador: {generator_order}")
@@ -153,36 +120,23 @@ if __name__=="__main__":
     P = Point(Qx, Qy, curve, check=False)  
     # si check=True (default) lanza excepción si el punto no está en la curva
 
-    # Generar claves (borrar clau privada)
-    d = 0xfb26a4e75eec75544c0f44e937dcf5ee6355c7176600b9688c667e5c283b43c5
-    private_key, public_key = generate_keypair(curve=curve, Q=P, d=d)
-    # print(f"Clave privada: {private_key}")
-    # print(f"Clave pública: {public_key}")
-    # print()
+    # Generar clave pública
+    public_key = generate_pubkey(Q=P)
+
 
     # Verificar si el punto público está en la curva
     print(f"Apartado b: ")
     print(f"La clave pública está en la curva?: {is_on_curve(curve, P)}")
     print()
 
+    # Mirar orden del punto
     print("Apartado c: ")
     print(f"Orden del punto de la clave pública: {point_order(P, curve)}")
     print()
 
-    # Crear y verificar una firma ECDSA (substituir per f1, f2 de Wireshark)
-    # (borrar)
-    # message = b"Este es un mensaje de prueba"
-    # ecdsa = ECDSA()
-    # signature = ecdsa.sign(message, private_key, curve)
-    # print(type(signature), len(signature))
-    # print(f"Firma: {signature}")
-    # # print()
-
-    print(message)
+    
     # Verificar la firma
     is_valid = verify_ecdsa_signature(public_key, message, f)
     print("Apartado d: ")
     print(f"¿Firma válida? {is_valid}")
     print()
-
-    # TLS.. --> 54 ... 79
